78e6fe1ff5d18861a18c8548c510af2e84bd6d04
Cairhiin <cairhiin@hotmail.com>
2017-07-19 22:11:58 +0300

    Added Saturation and Light Balance

--- colorFilters.js
+++ colorFilters.js
@@ -1,3 +1,97 @@
+let ImageFilters = Object.create(null);
+
+ImageFilters.pixelateImage = function pixelateImage(canvas) {
+  this.canvas = canvas;
+  this.cx = this.canvas.getContext('2d');
+  this.imgData = this.cx.getImageData(0, 0, this.cx.canvas.width, this.cx.canvas.height);
+};
+
+ImageFilters.applyFilter = function applyFilter(filter, amount) {
+  this.mutator = amount === undefined ? 100 : Number(amount);
+  return filter.call(this);
+};
+
+ImageFilters.sepia = function sepia() {
+  let colorRed;
+  let colorGreen;
+  let colorBlue;
+  this.mutator /= 100;
+  for (let i = 0; i < this.imgData.data.length; i += 4) {
+    colorRed = this.imgData.data[i];
+    colorGreen = this.imgData.data[i + 1];
+    colorBlue = this.imgData.data[i + 2];
+    this.imgData.data[i] = (colorRed * (1 - this.mutator)) + ((Math.min((colorRed * 0.393) +
+                        (colorGreen * 0.769) + (colorBlue * 0.189), 255)) * this.mutator);
+    this.imgData.data[i + 1] = (colorGreen * (1 - this.mutator)) + ((Math.min((colorRed * 0.349) +
+                        (colorGreen * 0.686) + (colorBlue * 0.168), 255)) * this.mutator);
+    this.imgData.data[i + 2] = (colorBlue * (1 - this.mutator)) + ((Math.min((colorRed * 0.272) +
+                        (colorGreen * 0.534) + (colorBlue * 0.131), 255)) * this.mutator);
+  }
+  this.cx.putImageData(this.imgData, 0, 0);
+  return this.canvas.toDataURL();
+};
+
+ImageFilters.grayScale = function grayscale() {
+  let avg;
+  this.mutator /= 100;
+  for (let i = 0; i < this.imgData.data.length; i += 4) {
+    avg = (this.imgData.data[i] + this.imgData.data[i + 1] + this.imgData.data[i + 2]) / 3;
+    this.imgData.data[i] = (this.mutator * avg) +
+          ((this.imgData.data[i]) * (1 - this.mutator));
+    this.imgData.data[i + 1] = (this.mutator * avg) +
+          ((this.imgData.data[i + 1]) * (1 - this.mutator));
+    this.imgData.data[i + 2] = (this.mutator * avg) +
+          ((this.imgData.data[i + 2]) * (1 - this.mutator));
+  }
+  this.cx.putImageData(this.imgData, 0, 0);
+  return this.canvas.toDataURL();
+};
+
+ImageFilters.inverse = function inverse() {
+  for (let i = 0; i < this.imgData.data.length; i += 4) {
+    this.imgData.data[i] = 255 - this.imgData.data[i];
+    this.imgData.data[i + 1] = 255 - this.imgData.data[i + 1];
+    this.imgData.data[i + 2] = 255 - this.imgData.data[i + 2];
+  }
+  this.cx.putImageData(this.imgData, 0, 0);
+  return this.canvas.toDataURL();
+};
+
+ImageFilters.lightBalance = function lightBalance() {
+  for (let i = 0; i < this.imgData.data.length; i += 4) {
+    this.imgData.data[i] = Math.min(255, this.imgData.data[i] + this.mutator);
+    this.imgData.data[i + 1] = Math.min(255, this.imgData.data[i + 1] + this.mutator);
+    this.imgData.data[i + 2] = Math.min(255, this.imgData.data[i + 2] + this.mutator);
+  }
+  this.cx.putImageData(this.imgData, 0, 0);
+  return this.canvas.toDataURL();
+};
+
+function truncateValue(value) {
+  let returnValue = value;
+  if (value < 0) {
+    returnValue = 0;
+  } else if (value > 255) {
+    returnValue = 255;
+  }
+  return returnValue;
+}
+
+ImageFilters.saturation = function saturation() {
+  const contrastFactor = (259 * (this.mutator + 255)) / (255 * (259 - this.mutator));
+  console.log(contrastFactor);
+  for (let i = 0; i < this.imgData.data.length; i += 4) {
+    this.imgData.data[i] =
+          truncateValue((contrastFactor * (this.imgData.data[i] - 128)) + 128);
+    this.imgData.data[i + 1] =
+          truncateValue((contrastFactor * (this.imgData.data[i + 1] - 128)) + 128);
+    this.imgData.data[i + 2] =
+          truncateValue((contrastFactor * (this.imgData.data[i + 2] - 128)) + 128);
+  }
+  this.cx.putImageData(this.imgData, 0, 0);
+  return this.canvas.toDataURL();
+}
+
 function loadImageURL(canvas, url) {
   let image = document.createElement('img');
   let cx = canvas.getContext('2d');
@@ -11,59 +105,11 @@ function loadImageURL(canvas, url) {
   image.src = url;
 }
 
-let ImageFilters = Object.create(null);
-
-ImageFilters.pixelateImage = function pixelateImage(canvas) {
-  this.canvas = canvas;
-  this.cx = this.canvas.getContext('2d');
-  this.imgData = this.cx.getImageData(0, 0, this.cx.canvas.width, this.cx.canvas.height);
-};
-
-ImageFilters.applyFilter = function applyFilter(filter, amount) {
-  this.fraction = amount === undefined ? 1 : amount;
-  return filter.call(this);
-};
-
-ImageFilters.sepia = function sepia() {
-  let colorRed;
-  let colorGreen;
-  let colorBlue;
-  for (let i = 0; i < this.imgData.data.length; i += 4) {
-    colorRed = this.imgData.data[i];
-    colorGreen = this.imgData.data[i + 1];
-    colorBlue = this.imgData.data[i + 2];
-    this.imgData.data[i] = (colorRed * (1 - this.fraction)) + ((Math.min((colorRed * 0.393) +
-                        (colorGreen * 0.769) + (colorBlue * 0.189), 255)) * this.fraction);
-    this.imgData.data[i + 1] = (colorGreen * (1 - this.fraction)) + ((Math.min((colorRed * 0.349) +
-                        (colorGreen * 0.686) + (colorBlue * 0.168), 255)) * this.fraction);
-    this.imgData.data[i + 2] = (colorBlue * (1 - this.fraction)) + ((Math.min((colorRed * 0.272) +
-                        (colorGreen * 0.534) + (colorBlue * 0.131), 255)) * this.fraction);
-  }
-  this.cx.putImageData(this.imgData, 0, 0);
-  return this.canvas.toDataURL();
-};
-
-ImageFilters.grayScale = function grayScale() {
-  let avg;
-  for (let i = 0; i < this.imgData.data.length; i += 4) {
-    avg = (this.imgData.data[i] + this.imgData.data[i + 1] + this.imgData.data[i + 2]) / 3;
-    this.imgData.data[i] = (this.fraction * avg) +
-          ((this.imgData.data[i]) * (1 - this.fraction));
-    this.imgData.data[i + 1] = (this.fraction * avg) +
-          ((this.imgData.data[i + 1]) * (1 - this.fraction));
-    this.imgData.data[i + 2] = (this.fraction * avg) +
-          ((this.imgData.data[i + 2]) * (1 - this.fraction));
-  }
-  this.cx.putImageData(this.imgData, 0, 0);
-  return this.canvas.toDataURL();
-};
-
 // Test code
 let canvas = document.querySelector('canvas');
 let cx = canvas.getContext('2d');
 let button = document.querySelectorAll('input')[2];
 let input = document.querySelectorAll('input')[0];
-
 
 input.addEventListener('change', function() {
   if (input.files.length === 0) return;
@@ -78,10 +124,11 @@ input.addEventListener('change', function() {
 
 button.addEventListener('click', function() {
   let image = document.querySelector('img');
-  let percentage = document.querySelectorAll('input')[1];
-  percentage = (percentage.value || 100) / 100;
+  let incrementer = document.querySelectorAll('input')[1];
+
+  incrementer = (incrementer.value || 100);
   image.src = '';
   ImageFilters.pixelateImage(canvas);
-  image.src = ImageFilters.applyFilter(ImageFilters.grayScale, percentage);
+  image.src = ImageFilters.applyFilter(ImageFilters.lightBalance, incrementer);
   canvas.style.display = 'none';
 });
