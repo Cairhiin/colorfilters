4e74977c4f63a69ec4d4cee6f60abd4f9b3763b4
Cairhiin <cairhiin@hotmail.com>
2017-07-19 15:41:00 +0300

    Reorganized the code

--- colorFilters.js
+++ colorFilters.js
@@ -11,45 +11,58 @@ function loadImageURL(canvas, url) {
   image.src = url;
 }
 
-function sepia(canvas, imgData, amount) {
-  let fraction = amount === undefined ? 1 : amount;
-  let cx = canvas.getContext('2d');
+let ImageFilters = Object.create(null);
+
+ImageFilters.pixelateImage = function pixelateImage(canvas) {
+  this.canvas = canvas;
+  this.cx = this.canvas.getContext('2d');
+  this.imgData = this.cx.getImageData(0, 0, this.cx.canvas.width, this.cx.canvas.height);
+};
+
+ImageFilters.applyFilter = function applyFilter(filter, amount) {
+  this.fraction = amount === undefined ? 1 : amount;
+  console.log(this.fraction);
+  return filter.call(this);
+};
+
+ImageFilters.sepia = function sepia() {
   let colorRed;
   let colorGreen;
   let colorBlue;
-  for (let i = 0; i < imgData.data.length; i += 4) {
-    colorRed = imgData.data[i];
-    colorGreen = imgData.data[i + 1];
-    colorBlue = imgData.data[i + 2];
-    imgData.data[i] = (colorRed * (1 - fraction)) + ((Math.min((colorRed * 0.393) +
-                        (colorGreen * 0.769) + (colorBlue * 0.189), 255)) * fraction);
-    imgData.data[i + 1] = (colorGreen * (1 - fraction)) + ((Math.min((colorRed * 0.349) +
-                        (colorGreen * 0.686) + (colorBlue * 0.168), 255)) * fraction);
-    imgData.data[i + 2] = (colorBlue * (1 - fraction)) + ((Math.min((colorRed * 0.272) +
-                        (colorGreen * 0.534) + (colorBlue * 0.131), 255)) * fraction);
+  for (let i = 0; i < this.imgData.data.length; i += 4) {
+    colorRed = this.imgData.data[i];
+    colorGreen = this.imgData.data[i + 1];
+    colorBlue = this.imgData.data[i + 2];
+    this.imgData.data[i] = (colorRed * (1 - this.fraction)) + ((Math.min((colorRed * 0.393) +
+                        (colorGreen * 0.769) + (colorBlue * 0.189), 255)) * this.fraction);
+    this.imgData.data[i + 1] = (colorGreen * (1 - this.fraction)) + ((Math.min((colorRed * 0.349) +
+                        (colorGreen * 0.686) + (colorBlue * 0.168), 255)) * this.fraction);
+    this.imgData.data[i + 2] = (colorBlue * (1 - this.fraction)) + ((Math.min((colorRed * 0.272) +
+                        (colorGreen * 0.534) + (colorBlue * 0.131), 255)) * this.fraction);
   }
-  cx.putImageData(imgData, 0, 0);
-  return canvas.toDataURL();
-}
+  this.cx.putImageData(this.imgData, 0, 0);
+  return this.canvas.toDataURL();
+};
 
-function grayScale(canvas, imgData, amount) {
-  let fraction = amount === undefined ? 1 : amount;
-  let cx = canvas.getContext('2d');
+ImageFilters.grayScale = function grayScale() {
   let avg;
-  for (let i = 0; i < imgData.data.length; i += 4) {
-    avg = (imgData.data[i] + imgData.data[i + 1] + imgData.data[i + 2]) / 3;
-    imgData.data[i] = (fraction * avg) + ((imgData.data[i]) * (1 - fraction));
-    imgData.data[i + 1] = (fraction * avg) + ((imgData.data[i + 1]) * (1 - fraction));
-    imgData.data[i + 2] = (fraction * avg) + ((imgData.data[i + 2]) * (1 - fraction));
+  for (let i = 0; i < this.imgData.data.length; i += 4) {
+    avg = (this.imgData.data[i] + this.imgData.data[i + 1] + this.imgData.data[i + 2]) / 3;
+    this.imgData.data[i] = (this.fraction * avg) +
+          ((this.imgData.data[i]) * (1 - this.fraction));
+    this.imgData.data[i + 1] = (this.fraction * avg) +
+          ((this.imgData.data[i + 1]) * (1 - this.fraction));
+    this.imgData.data[i + 2] = (this.fraction * avg) +
+          ((this.imgData.data[i + 2]) * (1 - this.fraction));
   }
-  cx.putImageData(imgData, 0, 0);
-  return canvas.toDataURL();
-}
+  this.cx.putImageData(this.imgData, 0, 0);
+  return this.canvas.toDataURL();
+};
 
+// Test code
 let canvas = document.querySelector('canvas');
 let cx = canvas.getContext('2d');
 let button = document.querySelectorAll('input')[2];
-let percentage = document.querySelectorAll('input')[1];
 let input = document.querySelectorAll('input')[0];
 
 
@@ -65,10 +78,12 @@ input.addEventListener('change', function() {
 });
 
 button.addEventListener('click', function() {
-  let data = cx.getImageData(0, 0, cx.canvas.width, cx.canvas.height);
   let image = document.querySelector('img');
+  let percentage = document.querySelectorAll('input')[1];
+  percentage = percentage.value / 100;
   image.src = '';
+  ImageFilters.pixelateImage(canvas);
+  image.src = ImageFilters.applyFilter(ImageFilters.grayScale, percentage);
   canvas.style.display = 'none';
   percentage = (percentage.value || 100) / 100;
-  image.src = sepia(canvas, data, percentage);
 });
